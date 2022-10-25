98b2691b7fd1eaba99c5395773d43b3b71350b52
Yamichann2003 <a.procza@gmail.com>
2022-10-25 22:35:00 +0200

    wadowice jan paweł drugi lubi cyce

---
+++ app/src/main/java/com/example/loginkino/RegisterActivity.java
@@ -0,0 +1,38 @@
+package com.example.loginkino;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private Button register;
+    private FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        auth = Firebase.Auth.getInstance();
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+                if (TextUtils.isEmpty(txt_email) Text)
+            }
+        });
+    }
+}
